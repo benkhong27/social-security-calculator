bb0ebb1aa443a6918baa07e938351970034836d1
benkhong27 <benkhong27@gmail.com>
2024-10-05 21:03:26 -0700

    add bullet points to second explanation

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,26 @@ function App() {
     <div style={{ display: "column" }}>
       <h1 className="title">Social Security Calculator</h1>
       <p className="explanation">
-        This Social Security calculator helps you find the best time to start
-        claiming your benefits. It estimates your total lifetime benefits for
-        different claiming ages (62 to 70), considering factors like your Full
-        Retirement Age, early claiming penalties, delayed retirement credits,
-        and potential benefit taxation if you're still working. The calculator
-        generates a graph showing your cumulative benefits over time, helping
-        you spot the "break-even" point and the age that maximizes your total
-        benefits. Using current Social Security rules and adjustable default
-        assumptions, it provides a clear picture of your options. This tool is
-        designed to help you make an informed decision about when to start your
-        retirement benefits, potentially maximizing your lifetime Social
-        Security income.
+        Get the most from Social Security. Our tool helps you find the best time
+        to claim benefits based on your situation.
+      </p>
+      <h2 className="subtitle">How this calculator works</h2>
+      <p className="explanation">
+        To estimate your lifetime Social Security benefits ("What you'll
+        receive"), start with your current age and your estimated benefit at
+        full retirement age. Our calculator shows how your benefits change if
+        you claim between ages 62 and 70. It factors in early claiming
+        penalties, delayed retirement credits, and potential benefit taxation if
+        you're still working. When calculating your total lifetime benefits, our
+        default assumptions include: A full retirement age based on your birth
+        year (between 66 and 67 for most people). An average life expectancy of
+        85 years. Annual cost-of-living adjustments of 2.5%. Potential reduction
+        in benefits if claiming before full retirement age while still working.
+        Increase in benefits for delaying claims past full retirement age. The
+        calculator generates a graph showing your cumulative benefits over time,
+        helping you identify the "break-even" point and the age that maximizes
+        your total benefits. You can adjust these assumptions in the "advanced
+        settings" to better match your personal situation.
       </p>
     </div>
   );
